b96238970b264a5df056be1885003324d50438df
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2021-09-05 17:17:44 +0530

    added encapsulation and abstraction notes

---
+++ 7_abstraction and encapsulation.py
@@ -0,0 +1,3 @@
+Abstraction in Python: Data abstraction in python and data encapsulation in python programming are related to each other. The main point that is necessary here to note, is that data abstraction is only possible to achieve through encapsulation.
+
+Encapsulation means storing or placing data in a single place to make it easily readable and compact in one place. Whereas data abstraction in python programming means to hide internal functionalities that are performing on the application using codes and to show only essential information (class attributes)
