0907021299a9b5307b0cc20fa6a639bb6bc4fc4d
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2021-09-08 07:17:07 +0530

    looking for multiple object

--- 18_operator_overloading_and Dunder method.py
+++ 18_operator_overloading_and Dunder method.py
@@ -9,11 +9,11 @@ class Employee:
         return f"Employee name with {self.name}, {self.salary} is set as {self.role}"
     
     def __add__(self,other):
-        return self.salary + other.salary
+        return self.salary + other.salary 
 
 e1 = Employee("Harry","Software Engineer", 80000)
 e2 = Employee("Anas","Manager", 70000)
-# e3 = Employee("Adeeb","Manager Senior", 90000)
+e3 = Employee("Adeeb","Manager Senior", 90000)
 
 print(e1+e2)
 # print(e1+e2+e3)  #problem, how to deal with three
